87581cf5f59f57c89e8f84194388f0ba6fcc0da8
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-11-09 10:12:13 +0900

    Extract ansible-galaxy roles list.

--- ansible-install.sh
+++ ansible-install.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-function install_roles() {
-  for role in dev-sec.ssh-hardening telusdigital.fish pixelart.chrome; do
-    ansible-galaxy install "${role}"
-  done
-}
-
 function main() {
   pip3 install -U pip ansible
-  install_roles
+  ansible-galaxy install -r requirements.yaml
 }
 
 main
